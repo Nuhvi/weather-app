f701aab80eb4cb4265792784e6a6650f5bff430a
nazeh <ar.nazeh@gmail.com>
2019-10-23 08:54:40 +0300

    added options for the fetchWeatherData function

--- src/js/api/fetch-weather-data.js
+++ src/js/api/fetch-weather-data.js
@@ -1,13 +1,18 @@
-const fetchWeatherData = async (cityName) => {
+const fetchWeatherData = async ({
+  cityName,
+  dataType = 'weather',
+  unitsFormat = 'metric',
+}) => {
   const API_KEY = '824cd72f73db619e1d3cb7ee720096c8';
-  const BASE_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
+  const BASE_ENDPOINT = 'https://api.openweathermap.org/data/2.5/';
 
+  const url = `${BASE_ENDPOINT}${dataType}?q=${cityName}
+  &appid=${API_KEY}&units=${unitsFormat}`;
+
+  console.log(url);
 
   try {
-    const response = await fetch(
-      `${BASE_ENDPOINT}?q=${cityName}&appid=${API_KEY}`,
-      { mode: 'cors' },
-    );
+    const response = await fetch(url, { mode: 'cors' });
     const weatherData = await response.json();
 
     if (`${weatherData.cod}` === '200') return weatherData;
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,11 @@ import '../scss/main.scss';
 import fetchWeatherData from './api/fetch-weather-data.js';
 
 const query = async () => {
-  const data = await fetchWeatherData('london');
+  const data = await fetchWeatherData({
+    cityName: 'london',
+    dataType: 'forecast',
+    unitsFormat: 'imperial',
+  });
   console.log(data);
 };
 
